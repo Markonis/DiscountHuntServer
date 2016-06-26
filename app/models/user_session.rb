ac136acd9d8a692326c19a2eb63cd15ea3a42732
Marko <contact.pavlovic@gmail.com>
2016-06-26 07:29:24 +0000

    Implement active? method on UserSession

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,5 +1,19 @@
 class UserSession < ActiveRecord::Base
   belongs_to :user
 
+  MAX_AGE = 30.minutes
+
   validates_presence_of :user, :token
+
+  def active?
+    older_than?(MAX_AGE)
+  end
+
+  def older_than?(duration)
+    age_in_seconds < duration
+  end
+
+  def age_in_seconds
+    ((DateTime.now - updated_at.to_datetime) * 86400).seconds
+  end
 end
--- test/models/user_session_test.rb
+++ test/models/user_session_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserSessionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "age_in_seconds" do
+    user_session = UserSession.new
+    user_session.updated_at = DateTime.now - 5.seconds
+    assert_in_delta 5.seconds, user_session.age_in_seconds, 0.1
+  end
+
+  test "active?" do
+    user_session = UserSession.new
+
+    user_session.updated_at = DateTime.now - (UserSession::MAX_AGE - 1.minute)
+    assert user_session.active?
+
+    user_session.updated_at = DateTime.now - (UserSession::MAX_AGE + 1.minute)
+    assert_not user_session.active?
+  end
 end
