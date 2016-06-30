40a3036002d1e73490ebf87a1803c982bf98e4a1
Marko <contact.pavlovic@gmail.com>
2016-06-30 09:08:34 +0000

    Implement DiscountSearch#perform_search

--- app/models/discount_search.rb
+++ app/models/discount_search.rb
@@ -1,3 +1,14 @@
 class DiscountSearch < ActiveRecord::Base
   belongs_to :user
+
+  validates_presence_of :query
+  before_create :set_result
+
+  def set_result
+    self.result = perform_search.to_json
+  end
+
+  def perform_search
+    Discount.where('title LIKE :search OR description LIKE :search OR category LIKE :search', search: "%#{query}%")
+  end
 end
--- test/models/discount_search_test.rb
+++ test/models/discount_search_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class DiscountSearchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "finds correct discounts" do
+    instance = DiscountSearch.new(query: "with")
+    result = instance.perform_search
+    assert_equal 2, result.count
+
+    instance = DiscountSearch.new(query: "Man")
+    result = instance.perform_search
+    assert_equal 1, result.count
+
+    instance = DiscountSearch.new(query: "Telephone")
+    result = instance.perform_search
+    assert_equal 0, result.count
+  end
 end
